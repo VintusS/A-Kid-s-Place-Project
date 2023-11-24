b5331dd37d2aa0fc9442e323b5d7e281a8ffe3da
VintusS <dragomir.mindrescu@isa.utm.md>
2023-11-25 01:04:28 +0200

    Initial Commit

---
+++ Human Traficking Hackathon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Human Traficking Hackathon
+//
+//  Created by Mindrescu Dragomir on 25.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Human Traficking Hackathon/Human_Traficking_HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Human_Traficking_HackathonApp.swift
+//  Human Traficking Hackathon
+//
+//  Created by Mindrescu Dragomir on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Human_Traficking_HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
